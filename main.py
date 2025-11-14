e59f9f1ff4c5667ef0e0829a6f3d8cc65cbc4f27
Stefano Nakata <stefano.nakata@gmail.com>
2025-11-14 11:48:15 +0000

    user menu

---
+++ main.py
@@ -0,0 +1,21 @@
+from application import register_user, login_user
+1
+def menu():
+    print("---Choose an option---")
+    print("1. Register")
+    print("2. Login")
+    print("3. Exit")
+
+def main():
+    while True:
+        menu()
+        choice = int(input("Enter choice: "))
+        if choice == 1:
+            register_user()
+        elif choice == 2:
+            login_user()
+        elif choice == 3:
+            print("See you next time.")
+            break
+
+main()
